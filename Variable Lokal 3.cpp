5d79910ac394cd0d6d9700ad207ef21ff7be526a
teguh2116 <43785306+teguh2116@users.noreply.github.com>
2019-01-06 06:25:50 +0700

    Add files via upload

---
+++ Variable Lokal 3.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+void cetak();
+int main()
+{
+    int a,b,t;
+    a=5 b=2;
+    t=a+b;
+    cetak();
+}
+void cetak()
+{
+    printf("%d",t)
+    //eroor
+    getch();
+
+}
